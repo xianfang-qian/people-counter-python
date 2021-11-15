5961a57ad27a4db2e6e623947dcb7c616fff1878
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-15 15:32:17 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,11 +35,11 @@ pip3 install numpy paho-mqtt jupyter
 #sudo 
 rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list
 #sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list.save
-curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-VERSION=node_6.x
-DISTRO="$(lsb_release -s -c)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+#curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+#VERSION=node_6.x
+#DISTRO="$(lsb_release -s -c)"
+#echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+#echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 #sudo apt-get update
 #sudo apt-get install nodejs
 
